4a37c2b58a6f46406309c37ff118c1d05ad42f91
BeratungsResistent <59779396+BeratungsResistent@users.noreply.github.com>
2020-09-29 08:46:55 +0200

    Update youtube_dl_extractor.py

--- tobrot/helper_funcs/youtube_dl_extractor.py
+++ tobrot/helper_funcs/youtube_dl_extractor.py
@@ -18,7 +18,7 @@ from tobrot import (
 )
 
 
-async def extract_youtube_dl_formats(url, yt_dl_user_name, yt_dl_pass_word, user_working_dir):
+async def extract_youtube_dl_formats(url, yt_dl_proxy, yt_dl_user_name, yt_dl_pass_word, user_working_dir):
     command_to_exec = [
         "youtube-dl",
         "--no-warnings",
@@ -30,9 +30,9 @@ async def extract_youtube_dl_formats(url, yt_dl_user_name, yt_dl_pass_word, user
         command_to_exec.append("--geo-bypass-country")
         command_to_exec.append("IN")
     #
-    #if yt_dl_proxy is not None:
-        #command_to_exec.append("--proxy")
-        #command_to_exec.append(yt_dl_proxy)
+    if yt_dl_proxy is not None:
+        command_to_exec.append("--proxy")
+        command_to_exec.append(yt_dl_proxy)
     if yt_dl_user_name is not None:
         command_to_exec.append("--username")
         command_to_exec.append(yt_dl_user_name)
