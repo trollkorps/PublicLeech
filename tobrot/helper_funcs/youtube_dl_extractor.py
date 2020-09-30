9ce3c30517840be5054e5878f0aea7526b2042e9
BeratungsResistent <59779396+BeratungsResistent@users.noreply.github.com>
2020-09-30 20:24:44 +0200

    Update youtube_dl_extractor.py

--- tobrot/helper_funcs/youtube_dl_extractor.py
+++ tobrot/helper_funcs/youtube_dl_extractor.py
@@ -153,8 +153,8 @@ async def extract_youtube_dl_formats(url, yt_dl_user_name, yt_dl_pass_word, user
             else:
                 format_id = current_r_json["format_id"]
                 format_ext = current_r_json["ext"]
-                cb_string_video = "{}|{}|{}|{}".format(
-                    "video", format_id, format_ext, "nosound"
+                cb_string_video = "{}|{}|{}|{}|{}".format(
+                    "video", format_id, format_ext, "noaudio", "hasvideo"
                 )
                 inline_keyboard.append([
                     InlineKeyboardButton(
