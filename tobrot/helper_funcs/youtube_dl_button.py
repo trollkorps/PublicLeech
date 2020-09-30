7a4232003d29599f02af2159b9eae69854aa6e59
BeratungsResistent <59779396+BeratungsResistent@users.noreply.github.com>
2020-09-30 07:21:55 +0200

    Update youtube_dl_button.py

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -117,13 +117,13 @@ async def youtube_dl_call_back(bot, update):
         for current_r_json in response_json:
             if "formats" in current_r_json:
                 for formats in current_r_json["formats"]:
-                    #LOGGER.info("////////HAT GEKLAPPT1////////")
+                    LOGGER.info("////////HAT GEKLAPPT1////////")
                     format_string = formats.get("format_note")
                     if format_string is None:
-                        #LOGGER.info("////////HAT GEKLAPPT2////////")
+                        LOGGER.info("////////HAT GEKLAPPT2////////")
                         format_string = formats.get("format")
                     if format_string is not None and "video only" in format_string and not "youtu" in youtube_dl_url:
-                        #LOGGER.info("////////HAT GEKLAPPT3////////")
+                        LOGGER.info("////////HAT GEKLAPPT3////////")
                         minus_f_format = youtube_dl_format + "+bestaudio"
         if "youtu" in youtube_dl_url:
             for for_mat in response_json["formats"]:
