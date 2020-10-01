123674970cc0163cb2549dd332f68237d555f216
BeratungsResistent <59779396+BeratungsResistent@users.noreply.github.com>
2020-10-01 10:33:45 +0200

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -51,7 +51,7 @@ async def incoming_purge_message_f(client, message):
 async def incoming_message_f(client, message):
     i_m_sefg = await message.reply_text("checking ", quote=True)
     LOGGER.info("TTget_markupTT")
-    t_, rm_ = await get_markup()
+    t_, rm_ = await get_markup(message)
     LOGGER.info(rm_)
     await i_m_sefg.edit_text(
         text=t_,
