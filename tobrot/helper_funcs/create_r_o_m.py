082daff9a62243f713ca2dff1f10c55842955a93
BeratungsResistent <59779396+BeratungsResistent@users.noreply.github.com>
2020-10-01 10:35:08 +0200

    Update create_r_o_m.py

--- tobrot/helper_funcs/create_r_o_m.py
+++ tobrot/helper_funcs/create_r_o_m.py
@@ -14,7 +14,8 @@ import configparser
 
 from pyrogram.types import (
     InlineKeyboardButton,
-    InlineKeyboardMarkup
+    InlineKeyboardMarkup,
+    Message
 )
 from tobrot import (
     R_CLONE_CONF_URI
@@ -24,51 +25,13 @@ from tobrot.helper_funcs.r_clone import (
 )
 
 
-async def get_markup():
+async def get_markup(message: Message):
     inline_keyboard = []
     ikeyboard = []
-    ikeyboard.append(InlineKeyboardButton(
-        "leech 🤔🤔",
-        callback_data=("leech").encode("UTF-8")
-    ))
     ikeyboard.append(InlineKeyboardButton(
         "youtube-dl",
         callback_data=("ytdl").encode("UTF-8")
     ))
-    inline_keyboard.append(ikeyboard)
-    ikeyboard = []
-    ikeyboard.append(InlineKeyboardButton(
-        "A leech TAR . GZ  🤔🤔",
-        callback_data=("leecha").encode("UTF-8")
-    ))
-    ikeyboard.append(InlineKeyboardButton(
-        "A youtube-dl TAR . GZ",
-        callback_data=("ytdla").encode("UTF-8")
-    ))
-    inline_keyboard.append(ikeyboard)
-    ikeyboard = []
-    if R_CLONE_CONF_URI:
-        r_clone_conf_file = await get_r_clone_config(
-            R_CLONE_CONF_URI,
-            message._client
-        )
-        if r_clone_conf_file is not None:
-            config = configparser.ConfigParser()
-            config.read(r_clone_conf_file)
-            remote_names = config.sections()
-            it_r = 0
-            for remote_name in remote_names:
-                ikeyboard.append(InlineKeyboardButton(
-                    f"RClone LEECH {remote_name}",
-                    callback_data=(f"leech_rc_{it_r}").encode("UTF-8")
-                ))
-                # ikeyboard.append(InlineKeyboardButton(
-                #     f"RClone YTDL {remote_name}",
-                #     callback_data=(f"ytdl_rc_{it_r}").encode("UTF-8")
-                # ))
-                inline_keyboard.append(ikeyboard)
-                ikeyboard = []
-                it_r = it_r + 1
     reply_markup = InlineKeyboardMarkup(inline_keyboard)
     inline_keyboard = []
 
